70c4dbd4faf0758c8db98ff2fee137cbef3ac8ed
frantan <francinetaing13@gmail.com>
2025-10-16 00:13:10 +0700

    feat: implement sidenav toggle

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import './style.css'
+
+// ===========================
+// # Toggle sidenav menu
+// ===========================
+const sidenavOpenBtn = document.getElementById('sidenav-open-btn');
+const sidenavCloseBtn = document.getElementById('sidenav-close-btn');
+const sidenav = document.getElementById('sidenav');
+const overlay = document.getElementById('sidenav-overlay');
+
+function openSidenav() { sidenav.classList.remove('translate-x-full'); overlay.classList.remove('hidden') }
+function closeSidenav() { sidenav.classList.add('translate-x-full'); overlay.classList.add('hidden') }
+
+sidenavOpenBtn.addEventListener('click', openSidenav);
+sidenavCloseBtn.addEventListener('click', closeSidenav);
+overlay.addEventListener('click', closeSidenav);
